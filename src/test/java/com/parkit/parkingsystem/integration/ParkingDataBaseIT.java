a7ea0a3e69fcd7704a6e395bf7c6ffe6490be0e6
Nugahh <nugah.dev@gmail.com>
2022-01-08 00:31:59 +0100

    Final version

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,6 +1,5 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -16,7 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 
@@ -48,12 +50,11 @@ public class ParkingDataBaseIT {
     }
 
     @AfterAll
-    private static void tearDown(){
-
+    private static void tearDown() {
     }
 
     @Test
-    public void testParkingACar(){
+    public void testParkingACar() {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
 
@@ -65,16 +66,21 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() {
+
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
 
         Ticket ticketResult = ticketDAO.getTicket("ABCDEF");
 
-        assertNotNull(ticketResult);
+        LocalDateTime outTime = LocalDateTime.now().plusHours(1);
+        ticketResult.setOutTime(outTime);
+        ticketDAO.saveTicket(ticketResult);
 
         parkingService.processExitingVehicle();
-        assertEquals(Fare.CAR_RATE_PER_HOUR * 0, ticketResult.getPrice());
+
+        assertNotNull(ticketResult.getPrice());
+        assertNotNull(ticketResult.getOutTime());
 
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
